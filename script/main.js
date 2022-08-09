6ab62dce32d0566b1c655fbae05e363d8512742a
PapiManu <45321589+PapiManu@users.noreply.github.com>
2022-08-09 09:27:32 +0200

    Finalisation defils

--- script/main.js
+++ script/main.js
@@ -1,3 +1,5 @@
+console.log("😎<<--PapiScript-->>")
+
 const body = document.body;
 
 const bl = "black"
@@ -5,25 +7,29 @@ const w = "white"
 const r = "red"
 const g = "green"
 const b = "blue"
-const p = "purple"
+const p = "pink"
 const y = "yellow"
 
 changeBackgroundcolor(bl)
 
 body.addEventListener("keydown", function(event) {
-    if (event.key = "r") {
+    if (event.key === "r") {
         changeBackgroundcolor(r)
     }
-    else if (event.key = "g") {
+    else if (event.key === "g") {
         changeBackgroundcolor(g)
     }
-    else if (event.key = "b") {
+    else if (event.key === "b") {
         changeBackgroundcolor(b)
     }
-    else if (event.key = "p") {
+    else if (event.key === "p") {
         changeBackgroundcolor(p)
     }
-}
+    else if (event.key === "y") {
+        changeBackgroundcolor(y)
+    }
+    else changeBackgroundcolor(w)
+})
 
 function changeBackgroundcolor(appletName) {
     body.style.backgroundColor = appletName
