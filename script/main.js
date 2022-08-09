d149c8303b057dd349accc8e3ddd728ea8d13165
PapiManu <45321589+PapiManu@users.noreply.github.com>
2022-08-09 10:01:25 +0200

    buger

---
+++ script/main.js
@@ -0,0 +1,12 @@
+console.log("😎<<--PapiScript-->>")
+
+const submit = document.getElementById("submit")
+
+submit.addEventListener("click", () => {
+    calculAge()
+})
+
+function calculAge() {
+    let naissance = document.getElementById("naissance").value
+
+}
